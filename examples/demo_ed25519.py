14f574e8ba7c1be77cf61af373ddabad60497f72
Ayain Fida <ayain.fida@outlook.com>
2026-02-17 10:32:32 +0000

    demo updated

--- examples/demo_ed25519.py
+++ examples/demo_ed25519.py
@@ -2,16 +2,16 @@ import argparse
 from ed25519 import ED25519, ED25519ScalarMultAlgorithm, Message
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Demo Ed25519 scalar multiplication algorithms")
-    parser.add_argument('--f', action='store_true', help='Use fast scalar multiplication algorithm')
+    parser = argparse.ArgumentParser(description="Demo Ed25519 signature generation and verification.")
+    parser.add_argument('--f', action='store_true', help='Use fast point addition')
     args = parser.parse_args()
 
     if args.f:
         algo = ED25519ScalarMultAlgorithm.FAST_SCALAR_MULT
-        print("Using fast scalar multiplication algorithm.")
+        print("Using fast point addition.")
     else:
         algo = ED25519ScalarMultAlgorithm.SCALAR_MULT
-        print("Using double-and-add algorithm.")
+        print("Using standard point addition.")
 
     ed25519_instance = ED25519(algo)
 
